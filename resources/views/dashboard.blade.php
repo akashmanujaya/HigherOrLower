e3206fb7e8ae9b0ae17c2ecd3685c00381c66c19
Akash Manujaya <akashmanujaya@gmail.com>
2023-11-10 12:08:15 +0000

    Frontend Design complete

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,40 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+@section('content')
+<div class="container" id="game_container">
+    <div class="row justify-content-center align-items-center text-center">
+        <div class="col-md-12 d-flex flex-column items-center">
+            <div class="game-status mb-2 text-white">
+                <span class="score fw-bolder">Score = 0</span>
+                <span class="lives fw-bolder">Lives = 3</span>
+                <div>
+                    <span class="high-score fw-bold">High Score = 3</span>
                 </div>
+                
             </div>
+
+            <div class="card">
+                <div class="card_top_left">
+                    2<br>
+                    <img src="{{ asset('assets/images/diamonds.png') }}" alt="Diamonds" class="card-suit">
+                </div>
+                <div class="card_suit d-flex justify-center">
+                    <img src="{{ asset('assets/images/diamonds.png') }}" alt="Diamonds" class="card-suit">
+
+                </div>
+                <div class="card_bottom_right">
+                    2<br>
+                    <img src="{{ asset('assets/images/diamonds.png') }}" alt="Diamonds" class="card-suit">
+                </div>
+
+            </div>
+            <p class="guess-prompt mb-3 text-white">Will the next card be higher or lower than the one above?</p>
+            <div class="guess-buttons mb-3">
+                <button type="button" class="btn btn-success">Higher</button>
+                <button type="button" class="btn btn-danger">Lower</button>
+            </div>
+            <button type="button" class="btn btn-primary">Shuffle Deck</button>
         </div>
     </div>
-</x-app-layout>
+</div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- CSS -->
+        @vite('resources/css/app.css')
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
@@ -20,17 +20,16 @@
 
             <!-- Page Heading -->
             @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
+                <header class="bg-dark dark:bg-gray-800 shadow">
                     <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                         {{ $header }}
                     </div>
                 </header>
             @endif
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
+            @yield('content')
+
+            @vite('resources/js/app.js')
         </div>
     </body>
 </html>
